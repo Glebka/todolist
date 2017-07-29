cecf17cfb3f033001b38d3f3e07214a1a25583d6
Hlieb Romanov <rgebox@gmail.com>
2017-07-29 23:28:13 +0300

    [model] Add TodoItemModel and TodoListModel classes

---
+++ src/js/models/TodoItemModel.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Model class that contains TODO item data
+ */
+class TodoItemModel {
+
+    /**
+     * Constructs new todo item with specified text
+     * @param {String} text - todo item text
+     */    
+    constructor(text) {
+        /**
+         * _id {Number} - database ID
+         */
+        this._id = null;
+
+        /**
+         * _text {String} - todo item text
+         */
+        this._text = text;
+
+        /**
+         * _state {Boolean} - state of todo item. If set to true - item is done
+         */
+        this._state = false;
+    }
+
+    /**
+     * Gets the text of todo item
+     * @returns {String} text of todo item
+     */    
+    getText() {
+        return this._text;
+    }
+
+    /**
+     * Gets the state of todo item
+     * @returns {Boolean} true - if todo item has been done, false - otherwise
+     */
+    getState() {
+        return this._state;
+    }
+
+    /**
+     * Sets the text of todo item
+     * @param  {String} text - new text of todo item
+     */
+    setText(text) {
+        this._text = text;
+    }
+
+    /**
+     * Sets the state of todo item
+     * @param {Boolean} state - true means done, false - means not finsihed
+     */
+    setState(state) {
+        this._state = state;
+    }
+
+}
---
+++ src/js/models/TodoListModel.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Model class that represents list of todo items
+ */
+class TodoListModel {
+    
+    constructor() {
+        /**
+         * _list {Array[TodoItemModel]} - container for TodoItemModel instances
+         */
+        this._list = [];
+    }
+
+    /**
+     * Constructs new todo item and adds it to the list
+     * @param {String} text - new todo item's text
+     */
+    addTodoItem(text) {
+        var item = new TodoItemModel(text);
+        this._list.push(item);
+    }
+
+    /**
+     * Gets todo item from the list by index
+     * @param {Number} index - todo item position in the list
+     * @returns {TodoItemModel} - requested todo item
+     */
+    getTodoItem(index) {
+        return this._list[index];
+    }
+
+    /**
+     * Returns length of the list
+     */
+    getLength() {
+        return this._list.length;
+    }
+
+    /**
+     * Removes todo item from the list by given index
+     * @param {Number} index - todo itme's index in the list
+     */
+    removeTodoItem(index) {
+        this._list = this._list.splice(index,1);        
+    }
+}
